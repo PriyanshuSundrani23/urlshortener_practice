14128fcacbb77fb546d2ebf4dc0e8e7f5b06a12d
PriyanshuSundrani23 <priyanshusundrani123@gmail.com>
2026-02-04 00:37:33 +0530

    ok

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 
+import userRouter from './router/user.routes.js'
+
 const app = express();
 const PORT = process.env.PORT ?? 8000;
+
+app.use(express.json());
+
+app.use('/user', userRouter);
 
 app.listen(PORT, () =>{
     console.log(`server is running on PORT ${PORT}`);
--- router/user.routes.js
+++ router/user.routes.js
@@ -3,29 +3,38 @@ import {db} from '../db/index.js';
 import { usersTable } from '../models/user.model.js';
 import {eq} from 'drizzle-orm';
 import {randomBytes, createHmac} from 'crypto'
+import { signupPostRequestBodySchema } from '../validation/request.validation.js';
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    const { firstname, lastname, email, password} = req.body;
+    const validationResult = await signupPostRequestBodySchema.safeParseAsync(req.body);
+    if(validationResult.error){
+        return res.status(400).json({error: validationResult.error.format() });
+    }
+
+    const {firstname, lastname, email, password} = validationResult.data;
 
     const [existingUser] = await db
     .select({
         id: usersTable.id,
     })
     .from(usersTable)
-    .where(eq(existingUser.email, email));
+    .where(eq(usersTable.email, email));
 
     if(existingUser) return res.status(400).json( {error: `user with email ${email} already exists!`});
 
     const salt = randomBytes(256).toString('hex');
-    const hashedPassword = createHmac('sha256', salt)
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
 
     const user = await db.insert(usersTable).values({
         email,
         firstname,
         lastname,
-    });
+        salt,
+        password: hashedPassword,
+    }).returning({ id: usersTable.id });
+    return res.status(201).json( {data: { userId: user[0].id }} )
 });
 
 export default router;
