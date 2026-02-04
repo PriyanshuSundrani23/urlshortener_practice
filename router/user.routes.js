2c0aaf6b33cfa00cb5ba8c61e5f6738f3566086a
PriyanshuSundrani23 <priyanshusundrani123@gmail.com>
2026-02-05 01:07:25 +0530

    refactoring

--- router/user.routes.js
+++ router/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';  
 import {db} from '../db/index.js';
 import { usersTable } from '../models/user.model.js';
-import {eq} from 'drizzle-orm';
-import {randomBytes, createHmac} from 'crypto'
-import { signupPostRequestBodySchema } from '../validation/request.validation.js';
+import { signupPostRequestBodySchema,loginPostRequestBodySchema } from '../validation/request.validation.js';
+import {hashPasswordWithSalt} from '../utils/hash.js'
+import {getUserByEmail} from '../services/user.service.js'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router();
 
@@ -14,18 +15,11 @@ router.post('/signup', async (req, res) => {
     }
 
     const {firstname, lastname, email, password} = validationResult.data;
-
-    const [existingUser] = await db
-    .select({
-        id: usersTable.id,
-    })
-    .from(usersTable)
-    .where(eq(usersTable.email, email));
+    const existingUser = await getUserByEmail(email);
 
     if(existingUser) return res.status(400).json( {error: `user with email ${email} already exists!`});
 
-    const salt = randomBytes(256).toString('hex');
-    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
+    const {salt, password: hashedPassword} = hashPasswordWithSalt(password);
 
     const user = await db.insert(usersTable).values({
         email,
@@ -36,5 +30,34 @@ router.post('/signup', async (req, res) => {
     }).returning({ id: usersTable.id });
     return res.status(201).json( {data: { userId: user[0].id }} )
 });
+
+router.post('/login', async(req, res)=>{
+
+    const loginvalidation = await loginPostRequestBodySchema.safeParseAsync(req.body);
+
+    if(loginvalidation.error){
+        return res.status(400).json({ error: loginvalidation.error.format() });
+    }
+
+    const { email, password } = loginvalidation.data;
+    const user = await getUserByEmail(email);
+
+    if(!user){
+        return res
+        .status(404)
+        .json({ error: `user with email ${email} does not exist`}); 
+    }
+
+    const { password: hashedPassword } = hashPasswordWithSalt(password, user.salt);
+
+    if(user.password !== hashedPassword){
+        return res.status(400).json({ error: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+    return res.json({ token });
+});
+
 
 export default router;
